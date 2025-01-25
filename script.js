e6f7662f41307956fd0442c405728e787c7f1e1b
Kevin Thiong'o <kevinthiongo99@gmail.com>
2025-01-25 15:31:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// A simple script to add interactivity (e.g., smooth scroll for navigation)
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+      });
+    });
+  });
+  
